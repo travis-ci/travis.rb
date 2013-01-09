babdf7c07863a395f9d41059bafb4330386d7cb4
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-01-09 19:06:37 +0100

    don't force authentication for endpoint command

--- lib/travis/cli/endpoint.rb
+++ lib/travis/cli/endpoint.rb
@@ -3,6 +3,10 @@ require 'travis/cli'
 module Travis
   module CLI
     class Endpoint < ApiCommand
+      def setup
+        # skip authentication on pro
+      end
+
       def run
         puts api_endpoint
       end
