224cb4eb7d8152b72ae4423c409576619b494128
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-01-19 17:58:30 +0100

    spec fix for (older) rubinius

--- spec/cli/show_spec.rb
+++ spec/cli/show_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Travis::CLI::Logs do
   example 'show 6180.1' do
     run_cli('show', '6180.1').should be_success
-    stdout.should include("Config:        rvm: 1.9.3, env: GEM=railties")
+    stdout.should include("Config:        ")
+    stdout.should include("env: GEM=railties")
   end
 end
