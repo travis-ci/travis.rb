1ad355deb540cec9c2af812c0beb2d57ab67e5ce
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-01-20 14:31:29 +0100

    fix spec description

--- spec/cli/show_spec.rb
+++ spec/cli/show_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Travis::CLI::Logs do
+describe Travis::CLI::Show do
   example 'show 6180.1' do
     run_cli('show', '6180.1').should be_success
     stdout.should include("Config:        ")
