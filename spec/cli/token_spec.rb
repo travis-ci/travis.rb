aaf5fb5cc3fb0f2802e043b1fe72e4f810fd02c3
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-01-24 11:53:01 +0100

    adjust tests for new token command behavior

--- spec/cli/token_spec.rb
+++ spec/cli/token_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe Travis::CLI::Token do
   example "travis token (access token set)" do
-    run_cli('token', '--t', 'super-secret').should be_success
+    run_cli('token', '-t', 'super-secret').should be_success
+    stdout.should == "super-secret\n"
+    stderr.should be_empty
+  end
+
+  example "travis token -i (access token set)" do
+    run_cli('token', '-it', 'super-secret').should be_success
     stdout.should == "Your access token is super-secret\n"
     stderr.should be_empty
   end
