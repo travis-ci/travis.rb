36525e06ff345fef9bb96d07d6b1edca93b49abb
Henrik Hodne <me@henrikhodne.com>
2013-07-11 13:00:01 -0700

    Initial annotation support

---
+++ lib/travis/client/annotation.rb
@@ -0,0 +1,15 @@
+require 'travis/client'
+
+module Travis
+  module Client
+    class Annotation < Entity
+      attributes :job_id, :provider_name, :image, :url, :description
+
+      # @!parse attr_reader :job
+      has :job
+
+      one :annotation
+      many :annotations
+    end
+  end
+end
