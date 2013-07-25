73d8520f2e89777a6ac8c98e6d3e56c5dcaf8403
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-07-25 11:02:52 +0200

    actually check .travis.yml

--- spec/cli/init_spec.rb
+++ spec/cli/init_spec.rb
@@ -19,20 +19,25 @@ describe Travis::CLI::Init do
 
   shared_examples_for 'travis init' do |language|
     example "travis init #{language} (empty directory)" do
+      File.exist?('.travis.yml').should be_false
       run_cli('init', language).should be_success
       stdout.should be == ".travis.yml file created!\n"
+      File.exist?('.travis.yml').should be_true
+      File.read('.travis.yml').should start_with("language: #{language}")
     end
 
     example "travis init #{language} (.travis.yml already exists, using --force)" do
-      File.open(".travis.yml", "w") {}
+      File.open(".travis.yml", "w") { |f| f << "old file" }
       run_cli('init', language, '--force').should be_success
       stdout.should be == ".travis.yml file created!\n"
+      File.read(".travis.yml").should_not be == "old file"
     end
 
     example "travis init #{language} (.travis.yml already exists, not using --force)" do
-      File.open(".travis.yml", "w") {}
+      File.open(".travis.yml", "w") { |f| f << "old file" }
       run_cli('init', 'ruby').should_not be_success
       stderr.should be == ".travis.yml already exists, use --force to override\n"
+      File.read('.travis.yml').should be == "old file"
     end
   end
 
