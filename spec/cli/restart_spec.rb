0095c7e0d82b8286f522abd80f236629b3197ae9
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-08-05 01:46:51 +0200

    fix specs

--- spec/cli/restart_spec.rb
+++ spec/cli/restart_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe Travis::CLI::Open do
   example 'travis restart' do
-    run_cli('restart').should be_success
+    run_cli('restart', '-t', 'token').should be_success
     $params['build_id'].should be == "4125095"
     $params['job_id'].should be_nil
   end
 
   example 'travis restart 6180.1' do
-    run_cli('restart', '6180.1').should be_success
+    run_cli('restart', '6180.1', '-t', 'token').should be_success
     $params['build_id'].should be_nil
     $params['job_id'].should be == "4125096"
   end
