2c7f60ddaa504e17da21b57d891ad5c47728f6c6
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-08-18 15:59:44 -0400

    make sure things work on jruby

--- spec/travis_spec.rb
+++ spec/travis_spec.rb
@@ -4,6 +4,7 @@ describe Travis do
   its(:api_endpoint) { should be == 'https://api.travis-ci.org/' }
 
   it 'has a nice inspect on entities' do
+    pending "does not work on JRuby" if defined? RUBY_ENGINE and RUBY_ENGINE == 'jruby'
     Travis::Repository.find('rails/rails').inspect.should be == "#<Travis::Repository: rails/rails>"
   end
 end
