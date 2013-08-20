81e62a2884f840d5d77d7bdfa25a8e63179b1bb8
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-08-20 12:02:12 -0400

    fix tests

--- spec/cli/help_spec.rb
+++ spec/cli/help_spec.rb
@@ -3,31 +3,31 @@ require 'spec_helper'
 describe Travis::CLI::Help do
   example "travis help" do
     run_cli('help').should be_success
-    stdout.should start_with("Usage: #$0 COMMAND")
+    stdout.should include("Usage: travis COMMAND")
   end
 
   example "travis --help" do
     run_cli('--help').should be_success
-    stdout.should start_with("Usage: #$0 COMMAND")
+    stdout.should include("Usage: travis COMMAND")
   end
 
   example "travis -h" do
     run_cli('-h').should be_success
-    stdout.should start_with("Usage: #$0 COMMAND")
+    stdout.should include("Usage: travis COMMAND")
   end
 
   example "travis -?" do
     run_cli('-?').should be_success
-    stdout.should start_with("Usage: #$0 COMMAND")
+    stdout.should include("Usage: travis COMMAND")
   end
 
   example "travis help endpoint" do
     run_cli('help', 'endpoint').should be_success
-    stdout.should start_with("Usage: #$0 endpoint [options]")
+    stdout.should include("Usage: travis endpoint [options]")
   end
 
   example "travis endpoint --help" do
     run_cli('endpoint', '--help').should be_success
-    stdout.should start_with("Usage: #$0 endpoint [options]")
+    stdout.should include("Usage: travis endpoint [options]")
   end
 end
