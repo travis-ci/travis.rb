cb581d471918be137147e03201f8af1cef1b03ae
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-08-28 17:54:54 +0200

    automatically find cloudfoundry target

--- lib/travis/cli/setup/cloud_foundry.rb
+++ lib/travis/cli/setup/cloud_foundry.rb
@@ -8,11 +8,13 @@ module Travis
 
         def run
           deploy 'provider' => 'cloudfoundry' do |config|
-            config['target']       = ask("Cloud Foundry target: ").to_s
-            config['username']     = ask("Cloud Foundry user name: ").to_s
-            config['password']     = ask("Cloud Foundry password: ") { |q| q.echo = "*" }.to_s
-            config['organization'] = ask("Cloud Foundry organization: ").to_s
-            config['space']        = ask("Cloud Foundry space: ").to_s
+            target_file = File.expand_path('.cf/target', Dir.home)
+            config['target']       ||= File.read(target_file).chomp if File.exist? target_file
+            config['target']       ||= ask("Cloud Foundry target: ").to_s
+            config['username']     ||= ask("Cloud Foundry user name: ").to_s
+            config['password']     ||= ask("Cloud Foundry password: ") { |q| q.echo = "*" }.to_s
+            config['organization'] ||= ask("Cloud Foundry organization: ").to_s
+            config['space']        ||= ask("Cloud Foundry space: ").to_s
           end
         end
       end
