9121048444a3c1d6ef2c398dfd86cb66a5eceaf9
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-10-21 22:42:11 +0200

    add description for logout command, fixes #110

--- lib/travis/cli/logout.rb
+++ lib/travis/cli/logout.rb
@@ -3,6 +3,8 @@ require 'travis/cli'
 module Travis
   module CLI
     class Logout < ApiCommand
+      description "deletes the stored API token"
+
       def run
         endpoint_config.delete('access_token')
         success("Successfully logged out!")
