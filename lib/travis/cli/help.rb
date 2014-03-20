23173565b62c94b97bbd73da0883e26432dcb513
Zearin <zearin@gonk.net>
2014-03-20 15:18:51 -0400

    Group help output by command type

--- lib/travis/cli/help.rb
+++ lib/travis/cli/help.rb
@@ -10,13 +10,38 @@ module Travis
           say CLI.command(command).new.help
         else
           say "Usage: travis COMMAND ...\n\nAvailable commands:\n\n"
-          commands.each { |c| say "\t#{color(c.command_name, :command).ljust(20)} #{color(c.description, :info)}" }
+          
+          commands.each do |group, commands|
+            say "  #{group}"
+            commands.each do |c|
+              say "\t#{color(c.command_name, :command).ljust(20)} #{color(c.description, :info)}"
+            end
+            say "\n"
+          end
+          
           say "\nrun `#$0 help COMMAND` for more infos"
         end
       end
 
       def commands
-        CLI.commands.sort_by { |c| c.command_name }
+        command_hash = {
+          'General Commands'    => [],
+          'API Commands'        => [],
+          'Repository Commands' => []
+        }
+        
+        CLI.commands.each do |cmd|
+          if cmd.superclass.equal? Travis::CLI::ApiCommand
+            command_hash['API Commands'].push cmd
+          elsif cmd.superclass.equal? Travis::CLI::RepoCommand
+            command_hash['Repository Commands'].push cmd
+          else
+            command_hash['General Commands'].push cmd
+          end
+        end
+        
+        # sort values for each key
+        command_hash.each { |key, commands| commands.sort_by &:command_name }
       end
     end
   end
