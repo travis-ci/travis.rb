d6af9fd3ab74cf6f80ff8f72f4ac9b768d51318a
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-03-31 15:42:10 -0700

    have `travis setup releases` generate token

--- lib/travis/cli/setup/releases.rb
+++ lib/travis/cli/setup/releases.rb
@@ -1,4 +1,5 @@
 require 'travis/cli/setup'
+require 'travis/tools/github'
 
 module Travis
   module CLI
@@ -8,8 +9,23 @@ module Travis
 
         def run
           deploy 'releases' do |config|
-            config['api_key'] = ask("GitHub Oauth Key(With repo or repo:public permission): ") { |q| q.echo = "*" }.to_s
+            github.with_token { |t| config['api_key'] = t }
             config['file'] = ask("File to Upload: ").to_s
+          end
+        end
+
+        def github
+          @github ||= begin
+            load_gh
+            Tools::Github.new(session.config['github']) do |g|
+              g.drop_token    = false
+              g.ask_login     = proc { ask("Username: ") }
+              g.ask_password  = proc { |user| ask("Password for #{user}: ") { |q| q.echo = "*" } }
+              g.ask_otp       = proc { |user| ask("Two-factor authentication code for #{user}: ") }
+              g.debug         = proc { |log| debug(log) }
+              g.after_tokens  = proc { g.explode = true and error("no suitable github token found") }
+              g.scopes        = org? ? ['public_repo'] : ['repo']
+            end
           end
         end
       end
