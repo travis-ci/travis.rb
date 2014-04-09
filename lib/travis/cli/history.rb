ad5046305d5bf18bd1de46feb0ad34464db66c1c
Carlos Palhares <me@xjunior.me>
2014-04-08 23:37:49 -0300

    add option to display committer on history

--- lib/travis/cli/history.rb
+++ lib/travis/cli/history.rb
@@ -10,6 +10,7 @@ module Travis
       on('-b', '--branch BRANCH', 'Only show history for the given branch')
       on('-l', '--limit LIMIT', 'Maximum number of history items')
       on('-d', '--date', 'Include date in output')
+      on('-c', '--committer', 'Include committer in output')
       on('--[no-]all', 'Display all history items')
 
       def run
@@ -39,6 +40,7 @@ module Travis
             date? && color(formatter.time(build.finished_at || build.started_at), build.color),
             color("##{build.number} #{build.state}:".ljust(16), [build.color, :bold]),
             color("#{build.branch_info}", :info),
+            committer? && build.commit.author_name.ljust(15),
             build.commit.subject
           ].compact.join(" ").strip + "\n"
         end
