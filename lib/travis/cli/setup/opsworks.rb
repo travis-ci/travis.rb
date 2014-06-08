1e6a879707be4a4f36d535414a71606f5f88067f
joshua-anderson <j@zatigo.com>
2014-06-08 08:27:33 -0700

    Opsworks: Fix migrate settings. Fixes #197

--- lib/travis/cli/setup/opsworks.rb
+++ lib/travis/cli/setup/opsworks.rb
@@ -11,7 +11,7 @@ module Travis
             config['access_key_id'] = ask("Access key ID: ").to_s
             config['secret_access_key'] = ask("Secret access key: ") { |q| q.echo = "*" }.to_s
             config['app-id'] = ask("App ID: ").to_s
-            on("Migrate the database? ", config, 'migrate' => true)
+            config['migrate'] = true if agree("Migrate the Database? ") { |q| q.default = 'no' }
 
             encrypt(config, 'secret_access_key') if agree("Encrypt secret access key? ") { |q| q.default = 'yes' }
           end
