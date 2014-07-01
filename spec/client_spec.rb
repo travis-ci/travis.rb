5e64c3d552b0e251bd021584c989144bc2138356
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-07-01 14:43:45 +0200

    add specs for string vs symbol option

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe Travis::Client do
   its(:new) { should be_a(Travis::Client::Session) }
+
+  it 'accepts string argument' do
+    Travis::Client.new('http://foo/').uri.should be == 'http://foo/'
+  end
+
+  it 'accepts options hash with string keys' do
+    Travis::Client.new('uri' => 'http://foo/').uri.should be == 'http://foo/'
+  end
+
+  it 'accepts options hash with symbol keys' do
+    Travis::Client.new(:uri => 'http://foo/').uri.should be == 'http://foo/'
+  end
 end
