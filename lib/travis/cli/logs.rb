718cc1fce282d414bcf93e50ac29870dafbbe2e9
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-07-07 14:40:20 +0200

    add --delete and --no-stream options logs command

--- lib/travis/cli/logs.rb
+++ lib/travis/cli/logs.rb
@@ -5,7 +5,11 @@ require 'travis/tools/system'
 module Travis
   module CLI
     class Logs < RepoCommand
+      attr_accessor :delete, :reason
       description "streams test logs"
+      on('-d', '--delete [REASON]', 'remove logs') { |c, reason| c.delete, c.reason = true, reason }
+      on('-f', '--force', 'do not ask user to confirm deleting the logs')
+      on('--[no-]stream', 'only print current logs, do not stream')
 
       def setup
         super
@@ -14,9 +18,29 @@ module Travis
 
       include Tools::SafeString
       def run(number = last_build.number)
+        self.stream = true if stream.nil?
         job ||= job(number) || error("no such job ##{number}")
+        delete ? delete_log(job) : display_log(job)
+      end
+
+      def delete_log(job)
+        unless force?
+          error "not deleting logs without --force" unless interactive?
+          error "aborted" unless danger_zone? "Do you really want to delete the build log for #{color(job.inspect_info, :underline)}?"
+        end
+
+        warn "deleting log for #{color(job.inspect_info, [:bold, :info])}"
+        job.delete_log(reason || {})
+      end
+
+      def display_log(job)
         info "displaying logs for #{color(job.inspect_info, [:bold, :info])}"
-        job.log.body { |part| print interactive? ? encoded(part) : clean(part) }
+        return print_log(job.log.body) unless stream?
+        job.log.body { |part| print_log(part) }
+      end
+
+      def print_log(part)
+        print interactive? ? encoded(part) : clean(part)
       end
 
       private
@@ -29,7 +53,7 @@ module Travis
         end
 
         def check_websocket
-          require 'websocket-native'
+          require 'websocket-native' if stream?
         rescue LoadError => e
           raise e if e.respond_to?(:path) and e.path != 'websocket-native'
           info "speed up log streaming by installing the websocket-native gem"
