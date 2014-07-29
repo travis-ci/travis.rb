65caa9754d57075c163017e6e9f06acaaa291265
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-07-29 19:18:21 +0200

    fix decrypt command

--- lib/travis/cli/encrypt_file.rb
+++ lib/travis/cli/encrypt_file.rb
@@ -67,7 +67,7 @@ module Travis
       end
 
       def decrypt_command(path)
-        "openssl aes-256-cbc -K $#{env_name(:key)} -iv $#{env_name(:key)} -in #{escape_path(path)} -out #{escape_path(decrypt_to)}"
+        "openssl aes-256-cbc -K $#{env_name(:key)} -iv $#{env_name(:iv)} -in #{escape_path(path)} -out #{escape_path(decrypt_to)}"
       end
 
       def set_env_vars
