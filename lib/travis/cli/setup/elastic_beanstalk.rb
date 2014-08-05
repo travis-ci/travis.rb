80597953ddacfd15ca4ed046ad0ac270356a9e79
joshua-anderson <j@zatigo.com>
2014-08-05 09:52:47 -0700

    Add 'travis setup elasticbeanstalk'

---
+++ lib/travis/cli/setup/elastic_beanstalk.rb
@@ -0,0 +1,23 @@
+require 'travis/cli/setup'
+
+module Travis
+  module CLI
+    class Setup
+      class ElasticBeanstalk < Service
+        description "deployment to AWS Elastic Beanstalk"
+
+        def run
+          deploy 'elasticbeanstalk' do |config|
+            config['access_key_id'] = ask("Access key ID: ").to_s
+            config['secret_access_key'] = ask("Secret access key: ") { |q| q.echo = "*" }.to_s
+            config['region'] = ask("Elastic Beanstalk region: ") {|q| q.default = 'us-east-1'}.to_s
+            config['app'] = ask("Elastic Beanstalk application name: ").to_s
+            config['env'] = ask("Elastic Beanstalk environment to update: ").to_s
+
+            encrypt(config, 'secret_access_key') if agree("Encrypt secret access key? ") { |q| q.default = 'yes' }
+          end
+        end
+      end
+    end
+  end
+end
