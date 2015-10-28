ce04fe2d3bce745fdffa86a6d6871efb7a399365
Henrik Hussfelt <henrik@henrik.se>
2015-10-28 20:10:54 +0100

    Change the config to local_dir

--- lib/travis/cli/setup/gcs.rb
+++ lib/travis/cli/setup/gcs.rb
@@ -12,7 +12,7 @@ module Travis
             config['secret_access_key'] = ask("Secret access key: ") { |q| q.echo = "*" }.to_s
             config['bucket'] = ask("Bucket: ").to_s
             local_dir = ask("Local project directory to upload (Optional): ").to_s
-            config['local-dir'] = local_dir unless local_dir.empty?
+            config['local_dir'] = local_dir unless local_dir.empty?
             encrypt(config, 'secret_access_key') if agree("Encrypt secret access key? ") { |q| q.default = 'yes' }
           end
         end
