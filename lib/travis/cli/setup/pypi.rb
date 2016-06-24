329e1e786ee515c6e198801558405ce4c390eff8
Jannis Leidel <jannis@leidel.info>
2016-06-24 14:13:54 +0200

    Add wheel distributions to config not the conditional section. Fix #373.

--- lib/travis/cli/setup/pypi.rb
+++ lib/travis/cli/setup/pypi.rb
@@ -10,10 +10,10 @@ module Travis
           deploy 'pypi', 'release' do |config|
             config['user']     ||= ask("Username: ").to_s
             config['password'] ||= ask("Password: ") { |q| q.echo = "*" }.to_s
-
             on("release only tagged commits? ", config, 'tags' => true)
             # the default of pypi `setup.py build` is the `sdist`
-            on("deploy as wheel file too? ", config, 'distributions' => 'sdist bdist_wheel')
+            deploy_wheels = agree("deploy as wheel file too? ") { |q| q.default = 'no' }
+            config['distributions'] = 'sdist bdist_wheel' if deploy_wheels
           end
         end
       end
