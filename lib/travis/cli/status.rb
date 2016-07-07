569f647ced8f8e5f9c5ec4c1a7a769454fbda8b0
Alper Kokmen <alperthereal@gmail.com>
2016-07-06 21:27:29 -0700

    support for specifying branch name in cli/status
    this adds supports for specifying a branch name for cli/status (i.e.
    travis status command).
    
    - updates status to determine the build by overriding last_build method
      and calling super if branch isn't provided.
    - specs for cli/status.
    - updates to travis.sh for autocomplete (generated by rake).

--- lib/travis/cli/status.rb
+++ lib/travis/cli/status.rb
@@ -5,6 +5,7 @@ module Travis
     class Status < RepoCommand
       description "checks status of the latest build"
 
+      on '-b', '--branch BRANCH',     'only check status for the given branch'
       on '-x', '--[no-]exit-code',    'sets the exit code to 1 if the build failed'
       on '-q', '--[no-]quiet',        'does not print anything'
       on '-p', '--[no-]fail-pending', 'sets the status code to 1 if the build is pending'
@@ -14,6 +15,13 @@ module Travis
         exit 1 if exit_code?    and last_build.unsuccessful?
         exit 1 if fail_pending? and last_build.pending?
       end
+
+      private
+
+        def last_build
+          return super unless branch?
+          repository.last_on_branch(branch)
+        end
     end
   end
 end
--- spec/cli/status_spec.rb
+++ spec/cli/status_spec.rb
@@ -21,6 +21,11 @@ describe Travis::CLI::Status do
     stdout.should be_empty
   end
 
+  example "travis status -b master" do
+    run_cli('status').should be_success
+    stdout.should be =="failed\n"
+  end
+
   example "travis status -i" do
     run_cli('status', '-i', '--skip-completion-check', '-r', 'travis-ci/travis.rb').should be_success
     stdout.should be == "build #6180 failed\n"
