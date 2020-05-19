dea862f6b26e79cc18cb0cd2fee996da03625f60
Hiro Asari <asari.ruby@gmail.com>
2020-05-19 08:51:01 -0400

    Group commands in `help` text by function

--- lib/travis/cli/help.rb
+++ lib/travis/cli/help.rb
@@ -5,18 +5,52 @@ module Travis
     class Help < Command
       description "helps you out when in dire need of information"
 
+      CommandGroup = Struct.new(:cmds, :header)
+
       def run(command = nil)
         if command
           say CLI.command(command).new.help
         else
+          api_cmds   = CommandGroup.new(api_commands,   'API commands')
+          repo_cmds  = CommandGroup.new(repo_commands,  'Repo commands')
+          other_cmds = CommandGroup.new(other_commands, 'non-API commands')
+
           say "Usage: travis COMMAND ...\n\nAvailable commands:\n\n"
-          commands.each { |c| say "\t#{color(c.command_name, :command).ljust(22)} #{color(c.description, :info)}" }
+          [other_cmds, api_cmds, repo_cmds].each do |cmd_grp|
+            say "    #{cmd_grp.header}"
+            cmd_grp.cmds.each do |cmd|
+              say "        #{color(cmd.command_name, :command).ljust(22)} #{color(cmd.description, :info)}"
+            end
+          end
           say "\nrun `#$0 help COMMAND` for more info"
         end
       end
 
       def commands
         CLI.commands.sort_by { |c| c.command_name }
+      end
+
+      def cmd_group_header(title)
+        say "    #{color(title, :green)}"
+      end
+
+      def api_commands
+        CLI.commands.select do |cmd|
+          cmd.ancestors.include?(CLI::ApiCommand) &&
+          !cmd.ancestors.include?(CLI::RepoCommand)
+        end.sort_by {|c| c.command_name}
+      end
+
+      def repo_commands
+        CLI.commands.select do |cmd|
+          cmd.ancestors.include? CLI::RepoCommand
+        end.sort_by {|c| c.command_name}
+      end
+
+      def other_commands
+        CLI.commands.select do |cmd|
+          !cmd.ancestors.include? CLI::ApiCommand
+        end.sort_by {|c| c.command_name}
       end
     end
   end
