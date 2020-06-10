fc0a46c7a3a729d6cc9d12dde61b2014a0df3ee1
Hiro Asari <asari.ruby@gmail.com>
2020-06-10 11:11:26 -0400

    Fix syntax

--- lib/travis/tools/assets.rb
+++ lib/travis/tools/assets.rb
@@ -6,7 +6,7 @@ module Travis
 
       def asset_path(file)
         File.expand_path(file, BASE).tap do |x|
-          raise Travis::Client::FileNotFound, "#{x} does not exist" unless File.exist?(x) }
+          raise Travis::Client::FileNotFound, "#{x} does not exist" unless File.exist?(x)
         end
       end
 
