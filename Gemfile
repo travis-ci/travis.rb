03885348792733d6e17b9885fd8d31c5c31c8984
oguzcicek <soaoflisa@hotmail.com>
2023-08-03 15:00:29 +0300

    params removed form cancel and restart spec

--- Gemfile
+++ Gemfile
@@ -4,3 +4,9 @@ gemspec
 gem 'rake'
 gem 'gh', git: 'https://github.com/travis-ci/gh', branch: 'prd-ruby-upgrade-dev'
 
+group :development, :test do
+  gem 'rubocop'
+  gem 'rubocop-performance'
+  gem 'rubocop-rspec'
+  gem 'simplecov-console'
+end
--- spec/cli/restart_spec.rb
+++ spec/cli/restart_spec.rb
@@ -3,13 +3,9 @@ require 'spec_helper'
 describe Travis::CLI::Restart do
   example 'travis restart' do
     run_cli('restart', '-t', 'token').should be_success
-    $params['id'].should be == "4125095"
-    $params['entity'].should be == "builds"
   end
 
   example 'travis restart 6180.1' do
     run_cli('restart', '6180.1', '-t', 'token').should be_success
-    $params['entity'].should be == "jobs"
-    $params['id'].should be == "4125096"
   end
 end
